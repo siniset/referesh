e0c9ebdfe5e3990d6dab85f530222a21936a0d54
ellaverak <ella.korkeaaho@gmail.com>
2022-11-28 19:51:55 +0200

    Update app.py

--- app/app.py
+++ app/app.py
@@ -4,11 +4,5 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config.from_object("app.config.Config")
 db = SQLAlchemy(app)
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-
-app = Flask(__name__)
-app.config.from_object("app.config.Config")
-db = SQLAlchemy(app)
 
 from app import routes
