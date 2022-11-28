cf72cae6386d1d726a30f262e06c0771511fa218
ellaverak <ella.korkeaaho@gmail.com>
2022-11-28 20:21:46 +0200

    Add app/save_reference

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,19 @@
-from flask import render_template
+from flask import render_template, request, redirect
 from app.app import app
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/save", methods=["POST"])
+def save():
+    if request.method == "POST":
+        author = request.form["author"]
+        name = request.form["name"]
+        year = request.form["year"]
+        publisher = request.form["publisher"]
+
+        #tässä tapahtuu sitten save_reference moduulin funktion kutsu
+        #ja talletus tapahtuu
+
+    return redirect("/")
---
+++ app/save_reference.py
@@ -0,0 +1,5 @@
+from app.app import db
+
+
+def save_reference(author, name, year, publisher):
+    pass
