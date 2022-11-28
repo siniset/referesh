efc7efbce20398c680e534b255a6536fa1043a78
Nuutti Varvikko <nvarvikko@gmail.com>
2022-11-28 15:02:04 +0200

    initialize flask

--- app/app.py
+++ app/app.py
@@ -1,6 +1,6 @@
-def run():
-    pass
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
-
-if __name__ == "__main__":
-    run()
+app = Flask(__name__)
+app.config.from_object("app.config.Config")
+db = SQLAlchemy(app)
--- tasks.py
+++ tasks.py
@@ -3,7 +3,7 @@ from invoke import task
 
 @task
 def start(ctx):
-    ctx.run("python3 -m app.app")
+    ctx.run("flask --app app.app:app run")
 
 
 @task
