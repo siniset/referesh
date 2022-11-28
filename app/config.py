ec44c544ed10bc1a791d877cae4864414faef23b
Nuutti Varvikko <nvarvikko@gmail.com>
2022-11-28 15:01:16 +0200

    add app config

---
+++ app/config.py
@@ -0,0 +1,8 @@
+from os import environ
+from dotenv import find_dotenv, load_dotenv
+
+load_dotenv(find_dotenv())
+
+class Config:
+    FLASK_ENV = "development"
+    SQLALCHEMY_DATABASE_URI = environ.get("DATABASE_URI")
