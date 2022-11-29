d06d888cacb41a9006286df49804b07a698682c3
ellaverak <ella.korkeaaho@gmail.com>
2022-11-29 12:00:02 +0200

    add functionality to app/save_reference.py

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from flask import render_template, request, redirect
 from app.app import app
+from app import save_reference
 
 @app.route("/")
 def index():
@@ -8,11 +9,11 @@ def index():
 @app.route("/save", methods=["POST"])
 def save():
     if request.method == "POST":
+        type = request.form["type"]
         author = request.form["author"]
-        name = request.form["name"]
+        book_name = request.form["name"]
         year = request.form["year"]
         publisher = request.form["publisher"]
 
-        #save_reference moduulin funktion kutsu --> talletus tietokantaan
-
-    return redirect("/")
+        if save_reference.save(type, author, book_name, year, publisher):
+            return redirect("/")
--- app/save_reference.py
+++ app/save_reference.py
@@ -1,5 +1,39 @@
+from flask import session
 from app.app import db
 
 
-def save_reference(author, name, year, publisher):
-    pass
+def save_book_fields(author, book_name, year, publisher, id):
+
+    fields = {"author": author, "book_name": book_name, "year": year, "publisher": publisher}
+
+    try:
+        for key in fields.keys():
+                sql = """INSERT INTO fields (name, content, reference_entry)
+                VALUES (:name, :content, :id)"""
+                db.session.execute(sql, {"name": key, "content": fields[key], "id": id})
+    except:
+        return False
+
+    return True
+
+
+def save(type, author=None, book_name=None, year=None, publisher=None):
+
+    try:
+
+        name = f"{author}, {year}"
+
+        sql = """INSERT INTO reference_entries (name, type, created_at)
+             VALUES (:name, :type, NOW()) RETURNING id"""
+        id = db.session.execute(sql, {"name": name, "type": type}).fetchone()[0]
+
+        if type == "book":
+            if not save_book_fields(author, book_name, year, publisher, id):
+                raise
+
+        db.session.commit()
+
+    except:
+        return False
+
+    return True
