41d57f5825ed9ff0f976088b994bfee6ebdc31c5
Nuutti Varvikko <nvarvikko@gmail.com>
2022-12-05 02:38:51 +0000

    fix invalid column name in model

--- app/models/reference.py
+++ app/models/reference.py
@@ -7,4 +7,4 @@ class Reference(db.Model):
     name = db.Column(db.Text, nullable=False)
     type = db.Column(db.Text, nullable=False)
     created_at = db.Column(db.DateTime(timezone=True), server_default=func.now())
-    field = db.relationship("Field")
+    fields = db.relationship("Field")
