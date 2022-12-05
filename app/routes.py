325f732f65d2cba87a6f7814fb5e89f99580fb5f
Nuutti Varvikko <nvarvikko@gmail.com>
2022-12-05 02:37:26 +0000

    add routes

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, request, redirect
+from flask import render_template, request, redirect, jsonify, abort
 from app.app import app
 from app import save_reference, get_references, delete_reference
 from app.controllers import reference_controller
@@ -10,28 +10,37 @@ def index():
     return render_template("index.html", references=references)
 
 
+@app.route("/references/<id>")
+def reference(id):
+    reference = reference_controller.get_one(id)
+
+    fields = {}
+    for field in reference.fields:
+        fields[field.name] = field.content
+
+    return fields
+
+
 @app.route("/save", methods=["POST"])
 def save():
-    if request.method == "POST":
-        type = request.form["type"]
-        author = request.form["author"]
-        book_name = request.form["name"]
-        year = request.form["year"]
-        publisher = request.form["publisher"]
-        reference_name = request.form["reference_name"]
+    name = request.form["name"]
+    type = request.form["type"]
 
-        if save_reference.save(type, author, book_name, year, publisher, reference_name):
-            return redirect("/")
-        else:
-            return render_template("error.html")
+    if len(name) == 0:
+        abort(400)
+
+    fields = {
+        "author": request.form["author"],
+        "title": request.form["title"],
+        "year": request.form["year"],
+        "publisher": request.form["publisher"]
+    }
+
+    reference_controller.create(name, type, fields)
+    return redirect("/")
 
 
-@app.route("/delete", methods=["POST"])
-def delete():
-    if request.method == "POST":
-        id = request.form["id"]
-
-        if delete_reference.delete(id):
-            return redirect("/")
-        else:
-            return render_template("error.html")
+@app.route("/delete/<id>", methods=["GET", "DELETE"])
+def delete(id):
+    reference_controller.delete_by_id(id)
+    return redirect("/")
