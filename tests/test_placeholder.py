ebc0aa9cf76c222d5e9dd2c80f5847a3ce446d81
unknown <juhosiren23@gmail.com>
2022-12-05 09:46:34 +0200

    lint works

--- tests/test_placeholder.py
+++ tests/test_placeholder.py
@@ -1,9 +1,9 @@
 import unittest
 
+
 class TestExample(unittest.TestCase):
 
     def test_placeholder(self):
         # avoids breaking CI when there are no tests
         # remove after real tests are added
         assert (5 == 5)
-        
