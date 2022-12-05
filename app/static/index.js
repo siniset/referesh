8b3cee07f22ef71268d4be9f4c0e261b645bae90
Nuutti Varvikko <nvarvikko@gmail.com>
2022-12-05 02:40:36 +0000

    WIP: enhance ui

---
+++ app/static/index.js
@@ -0,0 +1,38 @@
+const FieldContainer = document.createElement("div")
+FieldContainer.classList.add("reference-field-container", "vertical", "flex", "v-gap-lg")
+
+const deleteButton = document.createElement("a")
+deleteButton.classList.add("button", "button-dark", "text-bold")
+deleteButton.textContent = "Poista"
+FieldContainer.appendChild(deleteButton)
+
+const FieldView = document.createElement("div")
+FieldView.classList.add("field")
+
+async function toggleReferenceView(id) {
+  const referenceView = document.querySelector(`[data-reference-id='${id}']`)
+
+  if (referenceView.classList.toggle("open")) {
+    const referenceData = await fetchReferenceData(id)
+    const fieldContainer = FieldContainer.cloneNode(true)
+
+    fieldContainer.prepend(
+      ...Object.entries(referenceData).map(([name, content]) => {
+        const view = FieldView.cloneNode(false)
+        view.textContent = `${name}: ${content}`
+        return view
+      })
+    )
+
+    referenceView.appendChild(fieldContainer)
+    fieldContainer.lastChild.href = `/delete/${id}`
+  } else {
+    referenceView.querySelector(".reference-field-container").remove()
+  }
+}
+
+
+async function fetchReferenceData(id) {
+  const reference = await fetch(`/references/${id}`)
+  return reference.json()
+}
