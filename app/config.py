e647c23c923947f91ede90afc88ca441c299b046
Nuutti Varvikko <nvarvikko@gmail.com>
2022-12-06 03:55:49 +0200

    add db test uri in env variable

--- app/config.py
+++ app/config.py
@@ -1,10 +1,10 @@
 from os import environ
 from dotenv import find_dotenv, load_dotenv
 
-
 load_dotenv(find_dotenv())
 
-
 class Config:
-    FLASK_ENV = "development"
+    FLASK_ENV = environ.get("development") or "production"
     SQLALCHEMY_DATABASE_URI = environ.get("DATABASE_URL")
+    DATABASE_URL = environ.get("DATABASE_URL")
+    TEST_DATABASE_URL = environ.get("TEST_DATABASE_URL")
