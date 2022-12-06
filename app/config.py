2e5f64c6ad7a5133e0394587d094fd2887083313
Nuutti Varvikko <nvarvikko@gmail.com>
2022-12-06 04:21:42 +0200

    add testing tasks & update config

--- app/config.py
+++ app/config.py
@@ -4,7 +4,7 @@ from dotenv import find_dotenv, load_dotenv
 load_dotenv(find_dotenv())
 
 class Config:
-    FLASK_ENV = environ.get("development") or "production"
-    SQLALCHEMY_DATABASE_URI = environ.get("DATABASE_URL")
+    FLASK_ENV = environ.get("mode") or "production"
     DATABASE_URL = environ.get("DATABASE_URL")
     TEST_DATABASE_URL = environ.get("TEST_DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = environ.get("DATABASE_URL")
