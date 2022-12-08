8617ebd65355333cc610ed37f9e645eacfbfb6a5
temejin <93370315+temejin@users.noreply.github.com>
2022-12-08 21:15:18 +0200

    fix minor mistake in tests

--- tests/unit/test_export_service.py
+++ tests/unit/test_export_service.py
@@ -29,7 +29,7 @@ class TestExportService:
         reference.fields.append(Field(name="title",content="Title"))
         reference.fields.append(Field(name="year",content="2000"))
         reference.fields.append(Field(name="publisher",content="Publisher"))
-        return [reference]
+        return (reference,)
 
     def create_article_reference(self):
         reference = Reference(name="Test_article", type="article")
@@ -37,17 +37,17 @@ class TestExportService:
         reference.fields.append(Field(name="title",content="Title"))
         reference.fields.append(Field(name="year",content="2000"))
         reference.fields.append(Field(name="publisher",content="Publisher"))
-        return [reference]
+        return (reference,)
 
     def test_book_is_exported_correctly(self):
         references = self.create_book_reference()
-        f = export_service.export_as_bibtex((references,))
+        f = export_service.export_as_bibtex([references])
         content = f.read()
         assert bytes.decode(content) == "@book{Reference1,\n\tauthor = {Author},\n\ttitle = {Title},\n\tyear = {2000},\n\tpublisher = {Publisher},\n}\n\n"
 
     def test_article_is_exported_correctly(self):
         references = self.create_article_reference()
-        f = export_service.export_as_bibtex((references,))
+        f = export_service.export_as_bibtex([references])
         content = f.read()
         assert bytes.decode(content) == "@article{Test_article,\n\tauthor = {Author},\n\ttitle = {Title},\n\tyear = {2000},\n\tpublisher = {Publisher},\n}\n\n"
 
